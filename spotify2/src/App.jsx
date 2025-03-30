5ee3d844836e64f635526c6026b9e636bc1bea2c
vanshika <vanshika.jangamcg@gmail.com>
2025-03-30 23:48:35 +0530

    changed alignment

--- spotify2/src/App.jsx
+++ spotify2/src/App.jsx
@@ -3,13 +3,76 @@ import Sidebar from "./Sidebar"
 import './App.css'
 
 function App() {
- 
-
   return (
     <>
-    <div className="page1">
-      <Sidebar />
-      <Mainmenu />
+      <div className="page1">
+        <Sidebar />
+        <Mainmenu />
+        <div className="player-bar">
+          <div className="now-playing">
+            <img 
+              src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image3.png?raw=true" 
+              alt="Now playing" 
+              className="now-playing-img"
+            />
+            <div className="now-playing-info">
+              <div className="song-title">MORE</div>
+              <div className="artist">K/DA</div>
+            </div>
+          </div>
+          
+          <div className="player-controls">
+            <div className="control-buttons">
+              <button className="player-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M8 3a5 5 0 1 0 0 10A5 5 0 0 0 8 3z"/>
+                </svg>
+              </button>
+              <button className="player-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M4 4a.5.5 0 0 1 1 0v3.248l6.267-3.636c.54-.313 1.232.066 1.232.696v7.384c0 .63-.692 1.01-1.232.697L5 8.753V12a.5.5 0 0 1-1 0V4z"/>
+                </svg>
+              </button>
+              <button className="player-button play-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                </svg>
+              </button>
+              <button className="player-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M12.5 4a.5.5 0 0 0-1 0v3.248L5.233 3.612C4.693 3.3 4 3.678 4 4.308v7.384c0 .63.692 1.01 1.233.697L11.5 8.753V12a.5.5 0 0 0 1 0V4z"/>
+                </svg>
+              </button>
+              <button className="player-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
+                  <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
+                </svg>
+              </button>
+            </div>
+            
+            <div className="progress-bar">
+              <div className="progress-time">1:21</div>
+              <div className="progress-track">
+                <div className="progress-current"></div>
+              </div>
+              <div className="progress-time">3:45</div>
+            </div>
+          </div>
+          
+          <div className="volume-controls">
+            <button className="player-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+                <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+                <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
+              </svg>
+            </button>
+            <div className="progress-track" style={{ width: '80px', marginLeft: '8px' }}>
+              <div className="progress-current" style={{ width: '60%' }}></div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- spotify2/src/Mainmenu.jsx
+++ spotify2/src/Mainmenu.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function Mainmenu() {
   const boxes = [
     { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image9.png?raw=true', text: 'Weekly motivation' },
@@ -7,49 +9,90 @@ function Mainmenu() {
     { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image5.png?raw=true', text: 'The stories of Ma..' },
     { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image10.png?raw=true', text: 'Motivation Daily ba..' },
   ]
-  const boxes1   = [
+  
+  const boxes1 = [
     { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Liked.png?raw=true', text: 'Liked songs' },
     { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image4.png?raw=true', text: 'Neffex Playlist' },
     { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image3.png?raw=true', text: 'K/DA' },
     { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image1.png?raw=true', text: 'Dance/Electronic Mix' },
-  
   ]
+  
+  // Added recommended playlists section for more authentic Spotify look
+  const recommendedPlaylists = [
+    { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image2.png?raw=true', text: 'Today\'s Top Hits', description: 'Rema & Selena Gomez are on top of the Hottest 50!' },
+    { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image4.png?raw=true', text: 'RapCaviar', description: 'New music from Drake, Kendrick Lamar and more' },
+    { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image1.png?raw=true', text: 'All Out 2010s', description: 'The biggest songs of the 2010s.' },
+    { image: 'https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image5.png?raw=true', text: 'Rock Classics', description: 'Rock legends & epic songs that continue to inspire generations.' },
+  ]
+  
   return (
     <>
       <div className='main'>
-          <div className='header'>
-            <div><img src='https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/top%20arrow%20navigation.png?raw=true' alt='img' className='icon'></img></div>
-            <div><img src='https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/User.png?raw=true' className='user'></img></div>
+        <div className='header'>
+          <div className='navigation-controls'>
+            <img 
+              src='https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/top%20arrow%20navigation.png?raw=true' 
+              alt='navigation' 
+              className='icon'>
+            </img>
           </div>
-          <h2 className='good'>Good morning</h2>
-       
-            <div className='boxes1'>
-{boxes1.map((box, i) => (
-<div className='box'>
-  <img src={box.image} alt={box.text} className='like' />
-  <p className='txt'>{box.text}</p>
-</div>
-))}
-
+          <div className='user-controls'>
+            <img 
+              src='https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/User.png?raw=true' 
+              className='user'>
+            </img>
           </div>
-          <div className='box1'><img src='https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image2.png?raw=true' className='like'></img>
+        </div>
+        
+        <h2 className='good'>Good morning</h2>
+        
+        <div className='boxes1'>
+          {boxes1.map((box, i) => (
+            <div className='box' key={i}>
+              <img src={box.image} alt={box.text} className='like' />
+              <p className='txt'>{box.text}</p>
+            </div>
+          ))}
+          <div className='box'>
+            <img 
+              src='https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image2.png?raw=true' 
+              className='like'>
+            </img>
             <div className='txt'>Dance/Electronic Mix</div>
           </div>
-          <h2 className='show'>Shows You might like</h2>
-          <div className='boxes'>
-{boxes.map((box, i) => (
-<div className='box2'>
-  <img src={box.image} alt={box.text} className='pic' />
-  <p className='xyz'>{box.text}</p>
-</div>
-))} 
-
-</div>
         </div>
-    
+        
+        <div className='section-header'>
+          <h2 className='show'>Made For You</h2>
+          <span className='see-all'>SEE ALL</span>
+        </div>
+        
+        <div className='boxes'>
+          {recommendedPlaylists.map((playlist, i) => (
+            <div className='box2' key={i}>
+              <img src={playlist.image} alt={playlist.text} className='pic' />
+              <p className='xyz'>{playlist.text}</p>
+              <p className='description'>{playlist.description}</p>
+            </div>
+          ))}
+        </div>
+        
+        <div className='section-header'>
+          <h2 className='show'>Shows you might like</h2>
+          <span className='see-all'>SEE ALL</span>
+        </div>
+        
+        <div className='boxes'>
+          {boxes.map((box, i) => (
+            <div className='box2' key={i}>
+              <img src={box.image} alt={box.text} className='pic' />
+              <p className='xyz'>{box.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
 
 export default Mainmenu
-
